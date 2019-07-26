2c9fe8d10ffc33b3c5eae4e75a19b702cb18bed2
BaurzhanKadis <37457582+BaurzhanKadis@users.noreply.github.com>
2019-07-27 00:25:46 +0300

    commit first

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+// import Navbar from './components/Navbar';
+import Primary from './components/Primary';
+import Product from './components/Product';
+import styled from 'styled-components';
+import History from './components/History';
+import Showroom from './components/Showroom';
+import Contact from './components/Contact';
+import { Switch,Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Wrapper>
+        <Switch>
+          <Route exact path="/" component={Primary}/>
+          <Route path="/product" component={Product}/>
+          <Route path="/history" component={History}/>
+          <Route path="/showroom" component={Showroom}/>
+          <Route path="/contact" component={Contact}/>
+        </Switch>
+      </Wrapper>
+    </React.Fragment>
   );
 }
-
+const Wrapper = styled.div`
+  max-width: 1366px;
+  width: 100%;
+  margin: 0 auto;
+`;
 export default App;
